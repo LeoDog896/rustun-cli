43dbf33a95ebf2acff1e962c1947354a80256aba
LeoDog896 <LeoDog896@hotmail.com>
2022-11-15 19:15:51 +0000

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rustun::server::{BindingHandler, UdpServer};
+use anyhow::Result;
+
+enum STUNType {
+    Client,
+    Server
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    stun_type: STUNType,
+    #[clap(short, long, default_value_t = 3478)]
+    port: u16,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let addr = format!("0.0.0.0:{}", args.port).parse()?;
+
+    let server = fibers_global::execute(UdpServer::start(
+        fibers_global::handle(),
+        addr,
+        BindingHandler
+    ))?;
+    fibers_global::execute(server)?;
+    Ok(())
 }
